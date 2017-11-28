4136b1114f8580103c9bcc2af892d884f9b9de7f
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2017-11-28 16:19:05 +0800

    file-loader 控制 文件输出路径

--- webpack.common.js
+++ webpack.common.js
@@ -14,19 +14,34 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
-                    'file-loader',
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './static/',
+                        },
+                    },
                 ],
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
-                    'file-loader',
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './static/',
+                        },
+                    },
                 ],
             },
             {
                 test: /\.xml$/,
                 use: [
-                    'xml-loader',
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './static/',
+                        },
+                    },
                 ],
             },
         ],
