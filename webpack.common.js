0e2e6ce3507c35e3567f7b2a5a7c300ff57ed2ef
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2017-12-07 17:43:46 +0800

    css引用图片正常

--- webpack.common.js
+++ webpack.common.js
@@ -7,17 +7,17 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: '/static/woff/',
-                        },
-                    },
-                ],
-            },
+            // {
+            //     test: /\.(woff|woff2|eot|ttf|otf)$/,
+            //     use: [
+            //         {
+            //             loader: 'file-loader',
+            //             options: {
+            //                 outputPath: '/static/woff/',
+            //             },
+            //         },
+            //     ],
+            // },
             // {
             //     test: /\.xml$/,
             //     use: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -18,7 +18,17 @@ module.exports = merge(common, {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: ['css-loader'],
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            // root: '.',
+                            import: false,
+                            // modules: false,
+                            minimize: true,
+                            sourceMap: true,
+
+                        }
+                    }],
                     // publicPath: 'http://localhost:8088',
                 })
             },
